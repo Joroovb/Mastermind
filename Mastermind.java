851d037db9c76307fca6ebafd013e08a1c1672e5
Joroovb <jorisvanbreugel@gmail.com>
2020-06-09 15:03:56 +0200

    added .gitignore

--- Mastermind.java
+++ Mastermind.java
@@ -27,4 +27,15 @@ public class Mastermind {
 
 
     }
+public class Notificaties {
+
+        void spelStart() {
+            System.out.println( "Welkom bij Mastermind!\n" +
+                                "In dit spel moet je de code van de computer raden!\n" +
+                                "Het gaat om een code van vier letters.\n" +
+                                "Alleen de letters a, b, c, d, e & f zijn toegestaan.\n" +
+                                "Je hebt oneindig aantal pogingen, dus succes!");
+        }
+
+}
 }
